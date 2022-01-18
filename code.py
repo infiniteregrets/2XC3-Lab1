413bd1059ffd61c49a2d4bcbefb5db0ea06f0531
Mehul Arora <aroram18@mcmaster.ca>
2022-01-18 16:47:01 -0500

    valid_groups

---
+++ code.py
@@ -0,0 +1,21 @@
+from typing import List 
+def are_groups_valid(student_nums: List[int], groups: List[List[int]]) -> bool:
+    student_map = dict.fromkeys(student_nums, False)
+    for i in groups:
+        if len(i) not in [2,3]:
+                return False
+    for i in student_nums:
+        for j in groups:            
+            if student_map[i] and i in j:
+                return False
+            elif i in j:
+                student_map[i] = True 
+    return True 
+
+def test():
+    student_nums = [1,2,3,45]
+    groups = [[2,3], [1,45]]
+    assert(are_groups_valid(student_nums, groups) == True)
+test()                
+
+
